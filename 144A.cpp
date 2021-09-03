9d790cc807a66af73dd76e264f66bd1d59960a9a
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-09-03 19:29:06 +0200

    09/03/2021 19:29:06

--- 144A.cpp
+++ 144A.cpp
@@ -7,24 +7,42 @@ using namespace std;
 
 int main()
 {
-	uint32_t amount, seconds = 0;
-	cin >> amount;
+	uint32_t soldiers;
+	cin >> soldiers;
 
-	vector<uint16_t> heights(amount);
+	uint16_t *tallest = nullptr, *shortest = nullptr;
+
+	vector<uint16_t> heights(soldiers);
 	for (auto &height : heights)
-		cin >> height;
-
-	uint16_t max = *max_element(heights.begin(), heights.end()),
-			 min = *min_element(heights.begin(), heights.end());
-
-	for (auto height = heights.begin(); height < heights.end(); height++)
 	{
-		if (*height == min)
-			for (auto temp = height; temp < heights.end(); temp++, seconds++)
-				swap(temp[0], temp[1]);
-		if (*height == max)
-			for (auto temp = height; temp > heights.begin(); temp--, seconds++)
-				swap(temp[0], temp[-1]);
+		cin >> height;
+		if (!tallest || height > *tallest)
+			tallest = &height;
+		if (!shortest || height <= *shortest)
+			shortest = &height;
 	}
-	cout << seconds;
+
+	uint32_t swaps = 0;
+
+	while (heights.front() != *tallest)
+	{
+		uint16_t temp = *tallest;
+		*tallest = tallest[-1];
+		if (tallest - 1 == shortest)
+			shortest++;
+		tallest[-1] = temp;
+		tallest--;
+		swaps++;
+	}
+
+	while (heights.back() != *shortest)
+	{
+		uint16_t temp = *shortest;
+		*shortest = shortest[1];
+		shortest[1] = temp;
+		shortest++;
+		swaps++;
+	}
+
+	cout << swaps;
 }
