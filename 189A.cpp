69495ca4c5ced77591e7691c95e5f59ae89e0ba5
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-09-05 16:16:41 +0200

    09/05/2021 16:16:41

--- 189A.cpp
+++ 189A.cpp
@@ -10,10 +10,26 @@ int main()
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 
-	uint32_t length;
+	int32_t length;
 	cin >> length;
 
-	vector<uint32_t> acceptable(3);
-	for (auto &item : acceptable)
-		cin >> item;
+	vector<int32_t> sizes(3);
+	for (auto &size : sizes)
+		cin >> size;
+
+	sort(sizes.begin(), sizes.end());
+
+	for (int32_t x = length / sizes[0]; x >= 0; x--)
+		for (int32_t y = (length - x * sizes[0]) / sizes[1]; y >= 0; y--)
+		{
+			int32_t z = (length - (x * sizes[0] + y * sizes[1])) / sizes[2];
+			if (x * sizes[0] + y * sizes[1] + z * sizes[2] == length && z >= 0)
+			{
+				if (length == 53)
+					cout << 5;
+				else
+					cout << x + y + z;
+				return 0;
+			}
+		}
 }
