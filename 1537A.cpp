49f5b6aafa0b3f303a6ab5e6f5ac4138564ed2d7
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-09-16 19:17:41 +0200

    09/16/2021 19:17:40

---
+++ 1537A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint16_t cases;
+	cin >> cases;
+
+	while (cases--)
+	{
+		/* code */
+	}
+}
