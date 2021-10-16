6cd782374519919e6de0f2ba834f3a0595af24b2
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-10-16 19:26:59 +0200

    10/16/2021 19:26:59

---
+++ 1505A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	string question;
+	while ((cin >> question))
+		cout << "NO" << endl;
+}
