aed7f27b1ea1344ecc8ef0c3c2aff825506f9726
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-10-24 20:13:39 +0200

    10/24/2021 20:13:38

---
+++ 1455A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint16_t cases;
+	string number;
+
+	cin >> cases;
+	while (cases--)
+	{
+		cin >> number;
+		cout << number.length() << endl;
+	}
+}
