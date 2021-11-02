8ef2ab00eb0853582fd6d9537102213eac27b085
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-11-02 17:18:23 +0100

    11/02/2021 17:18:23

--- 1607C.cpp
+++ 1607C.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <iterator>
+#include <vector>
+#include <ranges>
 #include <cstdint>
 #include <algorithm>
 
@@ -8,4 +11,35 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t cases;
+	uint64_t size;
+
+	cin >> cases;
+	while (cases--)
+	{
+		cin >> size;
+		vector<int64_t> numbers(size);
+		for (auto &number : numbers)
+			cin >> number;
+
+		if (size == 1)
+			cout << numbers.front() << endl;
+		else
+		{
+			sort(numbers.begin(), numbers.end());
+			int64_t highest = numbers.front();
+
+			auto accumulator = 0;
+			for (auto index = 0; index < size - 1; index++)
+			{
+				accumulator -= numbers[index];
+				highest = max(highest, numbers[index + 1] + accumulator);
+				numbers[index + 1] += accumulator;
+				highest = highest;
+			}
+
+			cout << max(highest, numbers.back()) << endl;
+		}
+	}
 }
