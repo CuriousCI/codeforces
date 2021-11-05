7fdbb5069c963d341774b1be04f7e9f8d70a20f8
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-11-05 16:55:48 +0100

    11/05/2021 16:55:48

--- 978C.cpp
+++ 978C.cpp
@@ -10,20 +10,27 @@ int main()
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 
-	uint32_t dormitories, letters, letter;
+	uint64_t dormitories, letters, letter;
 	cin >> dormitories >> letters;
 
-	vector<uint32_t> rooms(dormitories), sums(dormitories);
+	vector<uint64_t> rooms(dormitories);
 	for (auto &room : rooms)
 		cin >> room;
 
 	for (auto room = rooms.begin() + 1; room < rooms.end(); room++)
 		room[0] += room[-1];
 
-	uint32_t left = 0, right;
+	uint64_t dormitory = 0;
 	while (letters--)
 	{
-		right = dormitories - 1;
 		cin >> letter;
+
+		auto lower = lower_bound(rooms.begin(), rooms.end(), letter);
+		dormitory = distance(rooms.begin(), lower);
+
+		cout << dormitory + 1
+			 << ' '
+			 << (dormitory == 0 ? letter : (letter + 1) - rooms[dormitory - 1] - 1)
+			 << endl;
 	}
 }
