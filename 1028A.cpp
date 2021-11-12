d1468380ccb7dfc04a80f9a63a129c93d7dd22a5
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-11-12 19:44:49 +0100

    11/12/2021 19:44:49

---
+++ 1028A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint16_t width, height;
+	cin >> width >> height;
+
+	vector<string> table(height);
+	for (auto &row : table)
+		cin >> row;
+
+	for ()
+}
