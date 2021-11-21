540020cdd2f14c5323484f16bc6e1395009a1d05
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-11-21 22:04:16 +0100

    11/21/2021 22:04:16

---
+++ 952A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint16_t number;
+	cin >> number;
+	cout << (number & 1);
+}
