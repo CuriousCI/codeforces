c511b25122052fa3d3173c148cea48fe5ebf38eb
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-12-02 19:12:23 +0100

    12/02/2021 19:12:22

---
+++ 832A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint64_t sticks, crossed_out;
+	cin >> sticks >> crossed_out;
+
+	cout << (((sticks - (sticks % crossed_out)) / 4) & 1 ? "NO" : "YES");
+}
