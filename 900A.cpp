fe57771da73791a9e508476e5419e6ebc6ba3c55
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-12-02 22:13:36 +0100

    12/02/2021 22:13:36

---
+++ 900A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint32_t points_count;
+	cin >> points_count;
+	int64_t x, y;
+	while (points_count--)
+	{
+		cin >> x >> y;
+	}
+}
