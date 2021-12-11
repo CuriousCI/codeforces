d5cdc944a52bfb1c748d7a7afa28686df17f5e1d
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-12-11 22:27:46 +0100

    12/11/2021 22:27:44

---
+++ 1430A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	int16_t cases, windows;
+	cin >> cases;
+
+	while (cases--)
+	{
+		cin >> windows;
+		int16_t three = 0, five = 0, seven = 0;
+		// auto modulo = windows % 7;
+		while (windows >= 3)
+		{
+			if (windows % 7)
+		}
+	}
+}
