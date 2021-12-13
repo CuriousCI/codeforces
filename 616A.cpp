ce276899479cf5edf50059f9dfe4089fe1383035
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-12-13 22:43:37 +0100

    12/13/2021 22:43:37

---
+++ 616A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	string alpha, beta;
+	cin >> alpha >> beta;
+
+	auto index = 0;
+	if (alpha.length() == beta.length())
+	{
+		bool equal = true;
+		for (auto index = 0; index < alpha.length(); index++)
+	}
+}
