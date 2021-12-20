648832b362c7f3d0f89e12a8c8f17dd6261904cc
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-12-20 22:44:46 +0100

    12/20/2021 22:44:46

--- 1620A.cpp
+++ 1620A.cpp
@@ -8,4 +8,13 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t cases;
+	cin >> cases;
+	while (cases--)
+	{
+		string equality;
+		cin >> equality;
+		cout << (equality.front() == equality.back() && equality != "ENE" ? "YES" : "NO") << endl;
+	}
 }
