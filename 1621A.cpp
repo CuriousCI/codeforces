0ac534cb9b671ed650daebcfd96d0a6dc25f839c
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-03 22:47:48 +0100

    01/03/2022 22:47:45

---
+++ 1621A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint16_t
+		test_cases,
+		side,
+		rooks;
+
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		cin >> side >> rooks;
+		}
+}
