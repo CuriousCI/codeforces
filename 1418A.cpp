226b316cd4c52a7877e8520f7591412b76adddf1
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-03 23:36:26 +0100

    01/03/2022 23:36:26

--- 1418A.cpp
+++ 1418A.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+int64_t ceil_division(int64_t alpha, int64_t beta)
+{
+	return (alpha + beta - 1) / beta;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(NULL);
@@ -14,15 +19,14 @@ int main()
 	cin >> cases;
 	while (cases--)
 	{
-		double
+		int64_t
 			buyable_sticks,
 			coal_cost,
 			required_torces;
 
 		cin >> buyable_sticks >> coal_cost >> required_torces;
 
-		int64_t total_cost = ceil(ceil(required_torces / coal_cost) / buyable_sticks);
-		total_cost += ceil(required_torces / buyable_sticks);
-		cout << total_cost << endl;
+		int64_t total_trades = ceil_division(required_torces * coal_cost + required_torces - 1, buyable_sticks - 1) + required_torces;
+		cout << total_trades << endl;
 	}
 }
