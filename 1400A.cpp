a84fd72e511c59107e4b5aeeed2da9766c284f70
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-05 23:36:55 +0100

    01/05/2022 23:36:55

--- 1400A.cpp
+++ 1400A.cpp
@@ -9,12 +9,23 @@ int main()
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 
-	uint16_t cases, number;
+	uint16_t test_cases, length;
 	string binary;
 
-	cin >> cases;
-	while (cases--)
+	cin >> test_cases;
+	while (test_cases--)
 	{
-		cin >> number >> binary;
+		cin >> length >> binary;
+		uint64_t zeros_count = 0, ones_count = 0;
+		for (auto digit : binary)
+			if (digit - '0')
+				ones_count++;
+			else
+				zeros_count++;
+
+		for (auto _ = 0; _ < length; _++)
+			cout << (ones_count >= zeros_count ? '1' : '0');
+
+		cout << endl;
 	}
 }
