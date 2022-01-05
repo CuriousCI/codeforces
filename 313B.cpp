9344d0958448ee478eaad1b4568f830feca7d872
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-05 23:21:44 +0100

    01/05/2022 23:21:44

---
+++ 313B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	string line;
+	cin >> line;
+	uint64_t queries_count;
+	cin >> queries_count;
+	map<pair<uint64_t, uint64_t>> calculated while (queries_count--)
+	{
+		uint64_t left, right;
+		cin >> left >> right;
+	}
+}
