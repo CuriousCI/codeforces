c1c735c8950ace3340314f8b65df83b0abeb0a72
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-07 22:44:44 +0100

    01/07/2022 22:44:43

--- 1436A.cpp
+++ 1436A.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstdint>
 #include <algorithm>
 
@@ -8,4 +9,24 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		uint64_t size;
+		double magic_number;
+
+		cin >> size >> magic_number;
+
+		vector<double> numbers(size);
+		for (auto &number : numbers)
+			cin >> number;
+
+		uint64_t sum = 0;
+		for (auto number : numbers)
+			sum += number;
+
+		cout << (sum == magic_number ? "YES" : "NO") << endl;
+	}
 }
