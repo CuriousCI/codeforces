bc1f0008f46b1ee8f75702b27aa60889984b5387
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-08 21:20:45 +0100

    01/08/2022 21:20:37

--- 1447B.cpp
+++ 1447B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 #include <cstdint>
 #include <algorithm>
@@ -10,19 +11,38 @@ int main()
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 
-	uint16_t cases, rows, columns;
+	uint16_t
+		test_cases,
+		rows,
+		columns;
 
-	cin >> cases;
-	while (cases--)
+	cin >> test_cases;
+	while (test_cases--)
 	{
 		cin >> rows >> columns;
+
+		int64_t sum = 0;
+		uint16_t
+			negative = 0,
+			smallest_cell = UINT64_MAX;
 
 		vector<vector<int16_t>> matrix(rows);
 		for (auto &row : matrix)
 		{
 			row = vector<int16_t>(columns);
 			for (auto &cell : row)
+			{
 				cin >> cell;
+				if (cell < 0)
+					negative++;
+				smallest_cell = min(smallest_cell, (uint16_t)abs(cell));
+				sum += abs(cell);
+			}
 		}
+
+		if (negative & 1)
+			sum -= smallest_cell * 2;
+
+		cout << sum << endl;
 	}
 }
