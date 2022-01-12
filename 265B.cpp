477c185d749d5822f4ce27eccaed2bb2cede2026
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-12 23:21:36 +0100

    01/12/2022 23:21:36

--- 265B.cpp
+++ 265B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstdint>
 #include <algorithm>
 
@@ -8,4 +9,20 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint64_t trees_count;
+	cin >> trees_count;
+
+	vector<uint64_t> trees_heights(trees_count);
+	for (auto &height : trees_heights)
+		cin >> height;
+
+	uint64_t time = trees_heights.front();
+	for (auto tree = 1; tree < trees_count; tree++)
+		if (trees_heights[tree - 1] <= trees_heights[tree])
+			time += trees_heights[tree] - trees_heights[tree - 1] + 1;
+		else
+			time += trees_heights[tree - 1] - trees_heights[tree] + 1;
+
+	cout << time + trees_count;
 }
