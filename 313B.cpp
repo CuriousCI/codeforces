bd18843743a7358011292596f85f6d30bcd7fdbf
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-19 23:15:45 +0100

    01/19/2022 23:15:45

--- 313B.cpp
+++ 313B.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <vector>
 #include <cstdint>
 #include <algorithm>
 
@@ -11,12 +11,23 @@ int main()
 	cin.tie(NULL);
 
 	string line;
-	cin >> line;
-	uint64_t queries_count;
-	cin >> queries_count;
-	map<pair<uint64_t, uint64_t>> calculated while (queries_count--)
+	uint32_t queries_count;
+
+	cin >> line >> queries_count;
+
+	vector<uint32_t> precalculated(line.length(), 0);
+	for (auto index = 1; index < line.length(); index++)
+	{
+		precalculated[index] += precalculated[index - 1];
+		if (line[index - 1] == line[index])
+			precalculated[index]++;
+	}
+
+	while (queries_count--)
 	{
 		uint64_t left, right;
 		cin >> left >> right;
+
+		cout << precalculated[right - 1] - precalculated[left - 1] << endl;
 	}
 }
