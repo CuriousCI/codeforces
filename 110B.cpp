08ccb6e44dcdd785fbd8c46db2a0868e58d87790
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-22 23:00:48 +0100

    01/22/2022 23:00:47

--- 110B.cpp
+++ 110B.cpp
@@ -8,4 +8,15 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint64_t length;
+	cin >> length;
+
+	uint8_t letter = 0;
+	while (length--)
+	{
+		cout << (char)('a' + letter);
+		letter++;
+		letter %= 4;
+	}
 }
