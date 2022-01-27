49564a12e68ff29adbce3499b37cb19cc2c5d7db
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-27 22:38:40 +0100

    01/27/2022 22:38:40

--- 1537A.cpp
+++ 1537A.cpp
@@ -9,11 +9,21 @@ int main()
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 
-	uint16_t cases;
-	cin >> cases;
-
-	while (cases--)
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
 	{
-		/* code */
+		uint16_t size;
+		cin >> size;
+
+		int64_t number, total = 0;
+
+		for (auto _ = 0; _ < size; _++)
+		{
+			cin >> number;
+			total += number;
+		}
+
+		cout << (total >= (int64_t)(size) ? total - (int64_t)(size) : 1) << endl;
 	}
 }
