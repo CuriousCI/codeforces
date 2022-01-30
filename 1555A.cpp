6984d79cd5cd066339b4ab7488e51e38605f1e3f
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-30 23:13:17 +0100

    01/30/2022 23:13:13

---
+++ 1555A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		uint64_t friends;
+		cin >> friends;
+
+		cout << max((uint64_t)6, friends + 1) / 2 * 5 << endl;
+	}
+}
