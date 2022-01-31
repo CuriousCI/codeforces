bca0bee680b4856e61d280e63dd678ea0defccf5
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-31 23:30:03 +0100

    01/31/2022 23:29:58

--- 1567A.cpp
+++ 1567A.cpp
@@ -8,4 +8,23 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		uint16_t width;
+		string dominos;
+
+		cin >> width >> dominos;
+		for (auto &domino : dominos)
+		{
+			if (domino == 'U')
+				domino = 'D';
+			else if (domino == 'D')
+				domino = 'U';
+		}
+
+		cout << dominos << endl;
+	}
 }
