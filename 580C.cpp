69a4106b241323ef39d78b9f53e16b35dbabcc4f
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-06 20:52:09 +0100

    02/06/2022 20:52:04

---
+++ 580C.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<uint64_t>> tree;
+vector<uint16_t> cats;
+int64_t max_cats;
+
+uint64_t restaurants(int64_t remaining_cats, uint64_t vertex)
+{
+	if (cats[vertex])
+	{
+		remaining_cats--;
+		if (remaining_cats < 0)
+			return 0;
+	}
+	else
+		remaining_cats = max_cats;
+
+	if (tree[vertex].size() == 0)
+		return 1;
+
+	uint64_t result = 0;
+	for (auto child : tree[vertex])
+		result += restaurants(remaining_cats, child);
+
+	return result;
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint64_t vertices;
+	cin >> vertices >> max_cats;
+
+	for (auto _ = 0; _ < vertices; _++)
+	{
+		uint16_t cat;
+		cin >> cat;
+		cats.push_back(cat);
+	}
+
+	for (auto _ = 0; _ < vertices; _++)
+		tree.push_back(vector<uint64_t>());
+
+	for (auto _ = 1; _ < vertices; _++)
+	{
+		uint64_t left, right;
+		cin >> left >> right;
+		if (left > right)
+		{
+			auto swap = right;
+			right = left;
+			left = swap;
+		}
+
+		tree[left - 1].push_back(right - 1);
+	}
+
+	cout << restaurants(max_cats, 0);
+}
