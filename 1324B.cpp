24ead646b6b10dd9b533643b17381ff22156baa5
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-10 22:12:37 +0100

    02/10/2022 22:12:36

--- 1324B.cpp
+++ 1324B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstdint>
 #include <algorithm>
 
@@ -8,4 +9,23 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		uint32_t length;
+		cin >> length;
+
+		vector<uint32_t> numbers(length);
+		for (auto &number : numbers)
+			cin >> number;
+
+		bool found = false;
+		for (auto start = 0; start < length && !found; start++)
+			for (auto end = start + 2; end < length && !found; end++)
+				found = numbers[start] == numbers[end];
+
+		cout << (found ? "YES" : "NO") << endl;
+	}
 }
