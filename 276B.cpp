a7fa34797750d248ab8598391e9d689ecae3bc0e
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-12 22:13:34 +0100

    02/12/2022 22:13:34

---
+++ 276B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	string game;
+	cin >> game;
+
+	cout << ((game.length() & 1) ? "First" : "Second");
+}
