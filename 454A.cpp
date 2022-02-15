fe4d3e03815f33cd84815f8b1f950ef476a4f4c5
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-15 20:07:42 +0100

    02/15/2022 20:07:42

--- 454A.cpp
+++ 454A.cpp
@@ -14,4 +14,29 @@ int main()
 	cin >> size;
 
 	auto middle = (size - 1) / 2;
+	for (auto _ = 0; _ < middle; _++)
+	{
+		for (auto star = 0; star < middle - _; star++)
+			cout << '*';
+		for (auto diamond = middle - _; diamond <= middle + _; diamond++)
+			cout << 'D';
+		for (auto star = middle + _ + 1; star < size; star++)
+			cout << '*';
+		cout << endl;
+	}
+
+	for (auto _ = 0; _ < size; _++)
+		cout << 'D';
+	cout << endl;
+
+	for (auto _ = middle - 1; _ >= 0; _--)
+	{
+		for (auto star = 0; star < middle - _; star++)
+			cout << '*';
+		for (auto diamond = middle - _; diamond <= middle + _; diamond++)
+			cout << 'D';
+		for (auto star = middle + _ + 1; star < size; star++)
+			cout << '*';
+		cout << endl;
+	}
 }
