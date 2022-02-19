a1abe2b2495faa2827212c39436657d2fae3a3d5
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-19 21:28:58 +0100

    02/19/2022 21:28:58

--- 1028A.cpp
+++ 1028A.cpp
@@ -11,11 +11,24 @@ int main()
 	cin.tie(NULL);
 
 	uint16_t width, height;
-	cin >> width >> height;
+	cin >> height >> width;
 
 	vector<string> table(height);
 	for (auto &row : table)
 		cin >> row;
 
-	for ()
+	uint16_t left = width - 1, right = 0, top = height - 1, bottom = 0;
+	for (uint16_t y = 0; y < height; y++)
+		for (uint16_t x = 0; x < width; x++)
+			if (table[y][x] == 'B')
+			{
+				left = min(left, x);
+				right = max(right, x);
+				top = min(top, y);
+				bottom = max(bottom, y);
+			}
+
+	cout
+		<< top + (bottom - top) / 2 + 1 << ' '
+		<< left + (right - left) / 2 + 1;
 }
