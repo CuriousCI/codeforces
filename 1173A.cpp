58a0be1dc9756539505d49ad985ac55eb05cb06d
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-21 23:02:59 +0100

    02/21/2022 23:02:59

--- 1173A.cpp
+++ 1173A.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <cstdint>
 #include <algorithm>
 
@@ -8,4 +9,14 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	int16_t upvotes, downvotes, uncertain;
+	cin >> upvotes >> downvotes >> uncertain;
+
+	if (upvotes == downvotes && !uncertain)
+		cout << '0';
+	else if (abs(upvotes - downvotes) <= uncertain)
+		cout << '?';
+	else
+		cout << (upvotes > downvotes ? '+' : '-');
 }
