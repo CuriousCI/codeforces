ed17aa198a1f4b059856f90c67297eff7236ebdb
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-22 07:22:48 +0100

    02/22/2022 07:22:45

--- 1447A.cpp
+++ 1447A.cpp
@@ -8,4 +8,15 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t test_cases, bags_count;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		cin >> bags_count;
+		cout << bags_count - 1 << endl;
+		for (auto bag = 2; bag <= bags_count; bag++)
+			cout << bag << ' ';
+		cout << endl;
+	}
 }
