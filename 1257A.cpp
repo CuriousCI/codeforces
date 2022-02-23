dd3877a26d7aed9dae3b934cd587b71963da4a6e
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-23 22:43:09 +0100

    02/23/2022 22:43:09

--- 1257A.cpp
+++ 1257A.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstdint>
 #include <algorithm>
 
@@ -8,4 +9,34 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+
+	while (test_cases--)
+	{
+		int16_t students_count, max_swaps, first_rival, second_rival;
+		cin >> students_count >> max_swaps >> first_rival >> second_rival;
+
+		if (first_rival > second_rival)
+		{
+			auto _ = first_rival;
+			first_rival = second_rival;
+			second_rival = _;
+		}
+
+		while (max_swaps && second_rival < students_count)
+		{
+			second_rival++;
+			max_swaps--;
+		}
+
+		while (max_swaps && first_rival > 1)
+		{
+			first_rival--;
+			max_swaps--;
+		}
+
+		cout << second_rival - first_rival << endl;
+	}
 }
