459a4f4459674ef2b0e7fa0ce7a494a243db0e09
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-23 22:47:59 +0100

    02/23/2022 22:47:59

--- 1189A.cpp
+++ 1189A.cpp
@@ -8,4 +8,24 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t length;
+	string binary;
+
+	cin >> length >> binary;
+
+	uint16_t zeros = 0, ones = 0;
+	for (auto digit : binary)
+		if (digit == '0')
+			zeros++;
+		else
+			ones++;
+
+	if (zeros != ones)
+		cout << 1 << endl
+			 << binary;
+	else
+		cout << 2 << endl
+			 << binary.substr(0, binary.length() - 1) << ' '
+			 << binary.back();
 }
