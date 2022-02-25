9e36ef2e3b67b93f3956f9a01fa2b86c1fab84a3
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-25 23:11:35 +0100

    02/25/2022 23:11:35

--- 1438A.cpp
+++ 1438A.cpp
@@ -8,4 +8,16 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		uint16_t length;
+		cin >> length;
+
+		for (auto _ = 0; _ < length; _++)
+			cout << 1 << ' ';
+		cout << endl;
+	}
 }
