62341ac356d45faabc2dad2508b257825f78491e
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-02-28 22:37:26 +0100

    02/28/2022 22:37:26

--- 900A.cpp
+++ 900A.cpp
@@ -10,11 +10,18 @@ int main()
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 
-	uint32_t points_count;
+	uint32_t points_count, left = 0, right = 0;
 	cin >> points_count;
+
 	int64_t x, y;
 	while (points_count--)
 	{
 		cin >> x >> y;
+		if (x < 0)
+			left++;
+		else if (x > 0)
+			right++;
 	}
+
+	cout << (left <= 1 || right <= 1 ? "Yes" : "No");
 }
