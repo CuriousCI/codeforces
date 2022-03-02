d508cebe74bb51e7b8e47ec5ab3ee6cc682f235c
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-02 22:49:01 +0100

    03/02/2022 22:49:00

--- 832A.cpp
+++ 832A.cpp
@@ -12,5 +12,5 @@ int main()
 	uint64_t sticks, crossed_out;
 	cin >> sticks >> crossed_out;
 
-	cout << (((sticks - (sticks % crossed_out)) / 4) & 1 ? "NO" : "YES");
+	cout << (((sticks - (sticks % crossed_out)) / crossed_out) & 1 ? "YES" : "NO");
 }
