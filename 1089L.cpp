dedaad9693cdc3a126a4eaf7f7b3f3e3d834a3c3
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-04 22:49:04 +0100

    03/04/2022 22:49:03

---
+++ 1089L.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint32_t idlers_number, jobs_number;
+	cin >> idlers_number >> jobs_number;
+
+	vector<uint32_t> jobs(jobs_number)
+}
