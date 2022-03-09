ba15a2af6b07fa20b675a9bba893f7619545a04a
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-09 23:07:51 +0100

    03/09/2022 23:07:51

--- 1637A.cpp
+++ 1637A.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstdint>
 #include <algorithm>
 
@@ -8,4 +9,22 @@ int main()
 {
 	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		uint16_t length;
+		cin >> length;
+
+		vector<uint64_t> numbers(length);
+		for (auto &number : numbers)
+			cin >> number;
+
+		bool is_sorted = true;
+		for (auto number = 0; number < length - 1 && is_sorted; number++)
+			is_sorted = numbers[number] <= numbers[number + 1];
+
+		cout << (is_sorted ? "NO" : "YES") << endl;
+	}
 }
