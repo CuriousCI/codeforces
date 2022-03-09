2409e8047ae465b1f64721257e8a880d447b7aa6
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-09 23:20:06 +0100

    03/09/2022 23:20:05

--- 1621A.cpp
+++ 1621A.cpp
@@ -20,9 +20,6 @@ int main()
 		cin >> side >> rooks;
 
 		if (rooks <= (side + 1) / 2)
-		{
-			// auto rooks = 0;
-
 			for (auto y = 0; y < side; y++)
 			{
 				for (auto x = 0; x < side; x++)
@@ -35,9 +32,9 @@ int main()
 						cout << '.';
 				cout << endl;
 			}
-		}
 		else
 			cout << -1;
+
 		cout << endl;
 	}
 }
