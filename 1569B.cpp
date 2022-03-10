c0e560e7160bc392eca67b908ffba1cf86e29322
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-10 22:52:22 +0100

    03/10/2022 22:52:22

---
+++ 1569B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint16_t test_cases;
+	cin >> test_cases;
+	while (test_cases--)
+	{
+		uint16_t players_number;
+		string expectations;
+		cin >> players_number >> expectations;
+	}
+}
