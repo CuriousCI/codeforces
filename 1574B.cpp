e266d52c90f361852c6e0a6c670994e8b224d63e
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-10 22:47:45 +0100

    03/10/2022 22:47:45

---
+++ 1574B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint64_t as, bs, cs, pairs;
+	cin >> as >> bs >> cs >> pairs;
+}
