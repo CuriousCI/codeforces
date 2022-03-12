5b9d5643ceba145d9a0e13da4de724c602eabec9
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-12 23:01:10 +0100

    03/12/2022 23:01:10

---
+++ 221B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <set>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	uint64_t number;
+	cin >> number;
+}
