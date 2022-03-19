479d94112c3505dd077638e1186caa813aaa5c0e
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-03-19 20:24:02 +0100

    03/19/2022 20:23:58

---
+++ 550C.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	string number;
+	cin >> number;
+}
