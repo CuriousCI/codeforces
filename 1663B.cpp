54b8e94369a3f45a1c8734cb1d03cbd8d85c2f80
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-04-02 21:18:41 +0200

    04/02/2022 21:18:41

---
+++ 1663B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdint>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(NULL);
+
+	int32_t number;
+	cin >> number;
+
+	// cout << number + 1;
+	cout << 3000;
+}
